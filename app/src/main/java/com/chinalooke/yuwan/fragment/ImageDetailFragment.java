67d656505f36d824157bbc7938396756a04fb036
xiaoqianxin33 <996584895@qq.com>
2016-12-10 16:20:23 +0800

    12-10

--- app/src/main/java/com/chinalooke/yuwan/fragment/ImageDetailFragment.java
+++ app/src/main/java/com/chinalooke/yuwan/fragment/ImageDetailFragment.java
@@ -1,6 +1,5 @@
 package com.chinalooke.yuwan.fragment;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,12 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.chinalooke.yuwan.R;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+import com.squareup.picasso.Picasso;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -73,46 +69,8 @@ public class ImageDetailFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        ImageLoader.getInstance().displayImage(mImageUrl, mImageView,
-                new SimpleImageLoadingListener() {
-                    @Override
-                    public void onLoadingStarted(String imageUri, View view) {
-                        progressBar.setVisibility(View.VISIBLE);
-                    }
-
-                    @Override
-                    public void onLoadingFailed(String imageUri, View view,
-                                                FailReason failReason) {
-                        String message = null;
-                        switch (failReason.getType()) {
-                            case IO_ERROR:
-                                message = "下载错误";
-                                break;
-                            case DECODING_ERROR:
-                                message = "图片无法显示";
-                                break;
-                            case NETWORK_DENIED:
-                                message = "网络有问题，无法下载";
-                                break;
-                            case OUT_OF_MEMORY:
-                                message = "图片太大无法显示";
-                                break;
-                            case UNKNOWN:
-                                message = "未知的错误";
-                                break;
-                        }
-                        Toast.makeText(getActivity(), message,
-                                Toast.LENGTH_SHORT).show();
-                        progressBar.setVisibility(View.GONE);
-                    }
-
-                    @Override
-                    public void onLoadingComplete(String imageUri, View view,
-                                                  Bitmap loadedImage) {
-                        progressBar.setVisibility(View.GONE);
-                        mAttacher.update();
-                    }
-                });
+        progressBar.setVisibility(View.GONE);
+        Picasso.with(getActivity()).load(mImageUrl).into(mImageView);
+        mAttacher.update();
     }
 }
