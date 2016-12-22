b89524981c3daecb6fa3fea839f96ec46923718d
xiaoqianxin33 <996584895@qq.com>
2016-12-22 18:25:28 +0800

    12-22

--- app/src/main/java/com/chinalooke/yuwan/receiver/MyReceiver.java
+++ app/src/main/java/com/chinalooke/yuwan/receiver/MyReceiver.java
@@ -1,11 +1,19 @@
 package com.chinalooke.yuwan.receiver;
 
-import android.content.BroadcastReceiver;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.support.v7.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.avos.avoscloud.AVBroadcastReceiver;
+import com.chinalooke.yuwan.R;
+import com.chinalooke.yuwan.activity.MyMessageActivity;
 import com.chinalooke.yuwan.bean.PushMessage;
 import com.chinalooke.yuwan.db.ExchangeHelper;
 import com.chinalooke.yuwan.utils.DateUtils;
@@ -16,9 +24,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-public class MyReceiver extends BroadcastReceiver {
-    public MyReceiver() {
-    }
+public class MyReceiver extends AVBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -34,6 +40,24 @@ public class MyReceiver extends BroadcastReceiver {
                 pushMessage.setDate(DateUtils.getCurrentDate());
                 pushDao.create(pushMessage);
                 pushDao.closeLastIterator();
+                Intent intent1 = new Intent(context, MyMessageActivity.class);
+                intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //PendingIntent主要用来处理即将发生的事,相当于Intent的延时,在这里是用来发送广播通知
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+                Notification notification = builder
+                        .setContentTitle(pushMessage.getTitle())//标题
+                        .setContentText(pushMessage.getContent())//内容
+                        .setWhen(System.currentTimeMillis())//通知时间，系统时间
+                        .setSmallIcon(R.mipmap.icon_512)//标题栏上显示的通知icon
+                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.icon_512))//通知显示的icon
+                        .setDefaults(Notification.DEFAULT_ALL)//DEFAULT_VIBRATE默认震动，DEFAULT_SOUND默认声音,DEFAULT_LIGHTS默认灯光
+                        .setColor(Color.parseColor("#98903B"))//smallIcon的背景色
+                        .setContentIntent(pendingIntent)
+                        .build();
+
+                manager.notify(1, notification);
             } catch (SQLException | IOException e) {
                 Log.e("TAG", e.getMessage());
             }
